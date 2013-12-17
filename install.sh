18517180e12a18a11307408fb830859a17f0d035
David Parlevliet <david@reactordigital.com.au>
2013-12-17 15:19:10 +1100

    oops, fixed path

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.github.com/dparlevliet/HAL/master/jbash -O /usr/local/bin/jbash
+wget https://raw.github.com/dparlevliet/jbash/master/jbash -O /usr/local/bin/jbash
 chmod +x /usr/local/bin/jbash
